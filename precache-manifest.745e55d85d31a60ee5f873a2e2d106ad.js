6e9c9dd1ab2eaf1a5fa70ff61b7e048b4ac52742
Anastasya Kushnir <anastasyakyshnir@gmail.com>
2020-11-25 11:19:08 +0200

    Updates

---
+++ precache-manifest.745e55d85d31a60ee5f873a2e2d106ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ec5ffe71da8a718af361aaae178fc0f",
+    "url": "/gallery/index.html"
+  },
+  {
+    "revision": "f4b03d9a36d9e6c8578f",
+    "url": "/gallery/static/css/main.4dd08b02.chunk.css"
+  },
+  {
+    "revision": "4dacd35006bbfad45a92",
+    "url": "/gallery/static/js/2.013b275a.chunk.js"
+  },
+  {
+    "revision": "4df4c58fd3db8af31000da1d75d059ea",
+    "url": "/gallery/static/js/2.013b275a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4b03d9a36d9e6c8578f",
+    "url": "/gallery/static/js/main.2ffbceb0.chunk.js"
+  },
+  {
+    "revision": "44b7b97f4bb21a2c27f0",
+    "url": "/gallery/static/js/runtime-main.997e86f1.js"
+  }
+]);
